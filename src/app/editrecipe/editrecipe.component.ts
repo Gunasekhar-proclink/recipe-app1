dacc542cb9392d6f6888ce18c510be1677007fb8
Guna Sekhar <gunasekhar.chinamallappa@proclink.com>
2024-08-02 10:47:04 +0530

    commit

---
+++ src/app/editrecipe/editrecipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editrecipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './editrecipe.component.html',
+  styleUrl: './editrecipe.component.scss'
+})
+export class EditrecipeComponent {
+
+}
